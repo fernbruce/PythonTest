39e8a498cba0f13a9f8a65a3a9b4b635a83ee1e6
fernbruce <171006308@qq.com>
2025-04-19 12:11:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ client = OpenAI(
     base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
 )
 
+#Add annotation
 def encode_image(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode("utf-8")
